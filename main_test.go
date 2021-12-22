497b8ae6cc936185f5733a72aa9adbc050553640
qxxt <57898942+qxxt@users.noreply.github.com>
2021-12-22 09:35:00 +0700

    add Sprintfm_usg test

--- main_test.go
+++ main_test.go
@@ -7,16 +7,30 @@ import (
 	"golang.org/x/term"
 )
 
-var sprintfm_lists = []struct {
+var sprintfm_tests = []struct {
 	margin int
 	text   string
 }{
 	{4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci."},
-	{10, "Donec vitae massa diam. Vivamus posuere blandit turpis id varius. Sed scelerisque turpis ut nisi suscipit, semper ultricies sapien interdum. Nulla id nulla varius, imperdiet urna at, ullamcorper tellus. Fusce mollis ornare nisi. Donec ultricies porta elit, quis consectetur tellus consequat vitae. Nullam vestibulum nec nibh in tincidunt. Aliquam sed felis et mi eleifend ultrices. Fusce at nisi laoreet erat consequat sodales sed id turpis. "},
-	{4, "Lorem ipsum"},
-	{10, "Donec vitae"},
-	{4, "Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-Sed-in-mattis-leo-Integer-eu-tortor-ut-libero-aliquet-dignissim-Etiam-nisi-metus-consectetur-eu-luctus-vel-malesuada-id-arcu-Integer-egestas-velit-a-velit-sollicitudin-venenatis-In-volutpat-nunc-posuere-ex-lobortis-maximus-Vivamus-fringilla-lacinia-nisi-nec-hendrerit-Duis-ipsum-tortor-congue-ut-est-eu-volutpat-pharetra-orci"},
-	{10, "Donec-vitae-massa-diam-Vivamus-posuere-blandit-turpis-id-varius-Sed-scelerisque-turpis-ut-nisi-suscipit-semper-ultricies-sapien-interdum-Nulla-id-nulla-varius-imperdiet-urna-at-ullamcorper-tellus-Fusce-mollis-ornare-nisi-Donec-ultricies-porta-elit-quis-consectetur-tellus-consequat-vitae-Nullam-vestibulum-nec-nibh-in-tincidunt-Aliquam-sed-felis-et-mi-eleifend-ultrices-Fusce-at-nisi-laoreet-erat-consequat-sodales-sed-id-turpis"},
+	{10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci. "},
+	{4, "Lorem ipsum dolor sit amet,"},
+	{10, "Lorem ipsum dolor sit amet,"},
+	{4, "LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci"},
+	{10, "LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci"},
+}
+
+var sprintfm_usg_tests = []struct {
+	arg string
+	msg string
+}{
+	{"--lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci."},
+	{"--lorem-ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci. "},
+	{"--lorem", "Lorem ipsum dolor sit amet,"},
+	{"--lorem-ipsum", "Lorem ipsum dolor sit amet,"},
+	{"--lorem", "LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci"},
+	{"--lorem-ipsum", "LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci"},
+	{"--lorem-ipsum-dolor-sit-amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci."},
+	{"--lorem-ipsum-dolor-sit-amet", "LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci"},
 }
 
 func TestTerminal(t *testing.T) {
@@ -40,8 +54,16 @@ func TestTerminal(t *testing.T) {
 }
 
 func t_fluits(terminal_width int, t *testing.T) {
-	for index, _ := range sprintfm_lists {
-		formatted, err := fmt_margin_wrap(sprintfm_lists[index].text, sprintfm_lists[index].margin, terminal_width)
+	for index, _ := range sprintfm_tests {
+		formatted, err := fmt_margin_wrap(sprintfm_tests[index].text, sprintfm_tests[index].margin, terminal_width)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf(formatted)
+	}
+	const max_args_length int = 20
+	for index, _ := range sprintfm_usg_tests {
+		formatted, err := sprintf_usg_verbose(sprintfm_usg_tests[index].arg, sprintfm_usg_tests[index].msg, max_args_length, terminal_width)
 		if err != nil {
 			fmt.Println(err)
 		}
