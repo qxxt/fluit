64d0a17442f683ec8c27ab8a7becddf63bdfd27b
qxxt <57898942+qxxt@users.noreply.github.com>
2021-12-31 19:35:24 +0700

    remove dependencies on tests

--- main_test.go
+++ main_test.go
@@ -3,8 +3,6 @@ package fluit
 import (
 	"fmt"
 	"testing"
-
-	"golang.org/x/term"
 )
 
 type sprintfUsgTest struct {
@@ -28,35 +26,21 @@ var sprintfUsg_Flag = []sprintfUsgTest{
 }
 
 func TestTerminal(t *testing.T) {
-	if term.IsTerminal(1) == false {
-		t.Errorf("error term.IsTerminal(): not running on terminal, or is not recognized as one.")
-	}
-	_, errGetstate := term.GetState(1)
-	if errGetstate != nil {
-		t.Errorf("error term.GetState(): %q.", errGetstate)
-	}
-	terminalWidth, _, errGetsize := term.GetSize(1)
-	if errGetsize != nil {
-		t.Errorf("error term.GetSize(): %q", errGetsize)
-		fmt.Printf("Terminal Width: unknown\r\n")
-		terminalWidth = 70
-	} else {
-		fmt.Printf("Terminal Width: %d\r\n", terminalWidth)
-	}
-	fmt.Println(Sprintfm("go-fluit is text formatter for wraping text, adding margin and building cli-usage. It use regex to split text based on specified breakpoint.", 0, terminalWidth))
-	fmt.Println(Sprintfm("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci.", 4, terminalWidth))
-	fmt.Println(Sprintfm("LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci", 4, terminalWidth))
-	fmt.Printf(Sprintfm("Avaliable commands:", 0, terminalWidth))
+	breakpoint := 70
+	fmt.Println(Sprintfm("go-fluit is text formatter for wraping text, adding margin and building cli-usage. It use regex to split text based on specified breakpoint.", 0, breakpoint))
+	fmt.Println(Sprintfm("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mattis leo. Integer eu tortor ut libero aliquet dignissim. Etiam nisi metus, consectetur eu luctus vel, malesuada id arcu. Integer egestas velit a velit sollicitudin venenatis. In volutpat nunc posuere ex lobortis maximus. Vivamus fringilla lacinia nisi nec hendrerit. Duis ipsum tortor, congue ut est eu, volutpat pharetra orci.", 4, breakpoint))
+	fmt.Println(Sprintfm("LoremipsumdolorsitametconsecteturadipiscingelitSedinmattisleoIntegereutortorutliberoaliquetdignissimEtiamnisimetusconsectetureuluctusvelmalesuadaidarcuIntegeregestasvelitavelitsollicitudinvenenatisInvolutpatnuncposuereexlobortismaximusVivamusfringillalacinianisinechendreritDuisipsumtortorcongueutesteuvolutpatpharetraorci", 4, breakpoint))
+	fmt.Printf(Sprintfm("Avaliable commands:", 0, breakpoint))
 	const maxCmdLength int = 10
 	for _, sprintfmUsgTest := range sprintfUsg_Cmd {
-		formatted := SprintfUsg(sprintfmUsgTest.arg, sprintfmUsgTest.msg, maxCmdLength, terminalWidth)
+		formatted := SprintfUsg(sprintfmUsgTest.arg, sprintfmUsgTest.msg, maxCmdLength, breakpoint)
 		fmt.Printf(formatted)
 	}
 	fmt.Println("")
-	fmt.Printf(Sprintfm("Avaliable flag:", 0, terminalWidth))
+	fmt.Printf(Sprintfm("Avaliable flag:", 0, breakpoint))
 	const maxFlagLength int = 5
 	for _, sprintfmUsgTest := range sprintfUsg_Flag {
-		formatted := SprintfUsg(sprintfmUsgTest.arg, sprintfmUsgTest.msg, maxFlagLength, terminalWidth)
+		formatted := SprintfUsg(sprintfmUsgTest.arg, sprintfmUsgTest.msg, maxFlagLength, breakpoint)
 		fmt.Printf(formatted)
 	}
 }
