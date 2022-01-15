bd9871f515ad1570f198d1117aec40cddf30f255
qxxt <57898942+qxxt@users.noreply.github.com>
2022-01-15 09:55:56 +0700

    add main example

---
+++ example1_test.go
@@ -0,0 +1,30 @@
+package fluit_test
+
+import (
+	"fmt"
+
+	"github.com/qxxt/fluit"
+)
+
+func Example() {
+	fluit.SetBreakpoint(70)
+	fmt.Print(fluit.Sprintwrap(4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim. Donec malesuada accumsan porttitor. In neque libero, sollicitudin vitae interdum ut, semper lacinia mauris. Integer auctor, nisl at commodo feugiat, justo urna varius arcu, a lacinia mi mauris sit amet purus."))
+
+	fmt.Print("\nUsages:\n")
+	u := fluit.Usgs{}
+	u.AddUsg("lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddUsg("ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddUsg("sit", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.AddUsg("amet", "Donec malesuada accumsan porttitor.")
+	u.AddUsg("consectetur", "Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.PrintUsg()
+
+	fmt.Print("\nAditional flags:\n")
+	u.SetArgLen(20)
+	u.AddUsg("--lorem string", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddUsg("--ipsum int", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddUsg("--sit=sits", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.AddUsg("--amet, -a", "Donec malesuada accumsan porttitor.")
+	u.AddUsg("--consectetur", "Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.PrintUsg()
+}
