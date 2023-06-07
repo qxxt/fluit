0d885474beac9d2fb7433a60a95268203aa10c83
qxxt <57898942+qxxt@users.noreply.github.com>
2023-06-08 00:10:41 +0700

    tidying up examples

--- example_test.go
+++ example_test.go
@@ -2,141 +2,35 @@ package fluit_test
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/qxxt/fluit"
 )
 
-func ExampleSetBreakpoint() {
-	fluit.SetBreakpoint(20)
-	fmt.Print(fluit.SprintfWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis."))
-	fmt.Print(fluit.SprintfWrap(5, strings.Repeat("-+", 5)))
+func Example() {
+	fluit.UserBreakpoint = 80
 
-	fluit.SetBreakpoint(60)
-	fmt.Print(fluit.SprintfWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis."))
-	fmt.Print(fluit.SprintfWrap(5, strings.Repeat("-+", 20)))
+	// if w, _, err := term.GetSize(1); err != nil {
+	//	fluit.UserBreakpoint = w
+	// }
 
-	// Output:
-	// Lorem ipsum dolor
-	// sit amet,
-	// consectetur
-	// adipiscing elit.
-	// Vivamus euismod
-	// pharetra sodales.
-	// Aenean ac massa
-	// dictum, gravida nisl
-	// non, fermentum
-	// turpis.
-	//      -+-+-+-+-+
-	// Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	// Vivamus euismod pharetra sodales. Aenean ac massa dictum,
-	// gravida nisl non, fermentum turpis.
-	//      -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-}
+	fluit.PrintlnWrap(4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim. Donec malesuada accumsan porttitor. In neque libero, sollicitudin vitae interdum ut, semper lacinia mauris. Integer auctor, nisl at commodo feugiat, justo urna varius arcu, a lacinia mi mauris sit amet purus.")
 
-func ExampleSprintfWrap() {
-	// In essence fluit is a string formatter. It don't prints.
-	str := fluit.SprintfWrap(4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis.")
-	fmt.Print(str)
-	fmt.Print(fluit.SprintfWrap(10, strings.Repeat("-+", 20)))
-
-	//If the margin is set to 0, it will not be margined but still wrapped according to breakpoint
-	fmt.Print(fluit.SprintfWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis."))
-
-	// Output:
-	//     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	//     Vivamus euismod pharetra sodales. Aenean ac massa
-	//     dictum, gravida nisl non, fermentum turpis.
-	//           -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	// Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	// Vivamus euismod pharetra sodales. Aenean ac massa dictum,
-	// gravida nisl non, fermentum turpis.
-}
-
-func ExampleSprintUsg() {
-	fmt.Print(fluit.SprintUsg(10, "--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante."))
-	fmt.Print(fluit.SprintUsg(10, "--build", "Nunc gravida metus non turpis sagittis, at lacinia magna tristique. Suspendisse vel ullamcorper eros. Sed eget lacinia urna."))
-	fmt.Print(fluit.SprintUsg(10, "--install", "Cras et tellus dignissim, tempor ligula nec, lobortis dui. In eu enim a urna cursus porta."))
-
-	// Output:
-	//   --help      Lorem ipsum dolor sit amet, consectetur
-	//               adipiscing elit. Integer quis aliquam ante.
-	//   --build     Nunc gravida metus non turpis sagittis, at
-	//               lacinia magna tristique. Suspendisse vel
-	//               ullamcorper eros. Sed eget lacinia urna.
-	//   --install   Cras et tellus dignissim, tempor ligula nec,
-	//               lobortis dui. In eu enim a urna cursus porta.
-}
-
-func ExampleUsages() {
-	var u fluit.Usages
-	u.SetArgLen(10)
-	u.AddUsg("--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.AddUsg("--build", "Nunc gravida metus non turpis sagittis, at lacinia magna tristique. Suspendisse vel ullamcorper eros. Sed eget lacinia urna.")
-	u.AddUsg("--install", "Cras et tellus dignissim, tempor ligula nec, lobortis dui. In eu enim a urna cursus porta.")
-	u.AddUsg("--some-very-long-flag", "Vivamus facilisis sagittis tristique. Quisque mattis sed libero id congue. Duis vel elit risus.")
-	u.AddUsg("--update", "Proin accumsan orci vel ante consequat, ut facilisis ipsum congue.")
-	u.PrintUsg()
-
-	// Output:
-	//   --help      Lorem ipsum dolor sit amet, consectetur
-	//               adipiscing elit. Integer quis aliquam ante.
-	//   --build     Nunc gravida metus non turpis sagittis, at
-	//               lacinia magna tristique. Suspendisse vel
-	//               ullamcorper eros. Sed eget lacinia urna.
-	//   --install   Cras et tellus dignissim, tempor ligula nec,
-	//               lobortis dui. In eu enim a urna cursus porta.
-	//   --some-very-long-flag
-	//               Vivamus facilisis sagittis tristique. Quisque
-	//               mattis sed libero id congue. Duis vel elit
-	//               risus.
-	//   --update    Proin accumsan orci vel ante consequat, ut
-	//               facilisis ipsum congue.
-}
-
-func ExampleUsages_SetArgLen() {
+	fmt.Println("Usages:")
 	u := fluit.Usages{}
-	u.AddUsg("--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.AddUsg("--dereference-command-line-symlink-to-dir", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.AddUsg("--another-help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.PrintUsg()
+	u.AddOption("lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddOption("ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddOption("sit", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.AddOption("amet", "Donec malesuada accumsan porttitor.")
+	u.AddOption("consectetur", "Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.PrintUsages()
+	u.UsageGroup = nil
 
-	u.SetArgLen(14)
-	u.AddUsg("--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.AddUsg("--dereference-command-line-symlink-to-dir", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.AddUsg("--another-help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
-	u.PrintUsg()
-
-	// Output:
-	//   --help                                     Lorem ipsum
-	//                                              dolor sit amet,
-	//                                              consectetur
-	//                                              adipiscing
-	//                                              elit. Integer
-	//                                              quis aliquam
-	//                                              ante.
-	//   --dereference-command-line-symlink-to-dir  Lorem ipsum
-	//                                              dolor sit amet,
-	//                                              consectetur
-	//                                              adipiscing
-	//                                              elit. Integer
-	//                                              quis aliquam
-	//                                              ante.
-	//   --another-help                             Lorem ipsum
-	//                                              dolor sit amet,
-	//                                              consectetur
-	//                                              adipiscing
-	//                                              elit. Integer
-	//                                              quis aliquam
-	//                                              ante.
-	//   --help          Lorem ipsum dolor sit amet, consectetur
-	//                   adipiscing elit. Integer quis aliquam
-	//                   ante.
-	//   --dereference-command-line-symlink-to-dir
-	//                   Lorem ipsum dolor sit amet, consectetur
-	//                   adipiscing elit. Integer quis aliquam
-	//                   ante.
-	//   --another-help  Lorem ipsum dolor sit amet, consectetur
-	//                   adipiscing elit. Integer quis aliquam
-	//                   ante.
+	fmt.Print("\nAditional flags:\n")
+	u.ArgumentRowLength = 20
+	u.AddOption("--lorem string", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddOption("--ipsum int", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+	u.AddOption("--sit=sits", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.AddOption("--amet, -a", "Donec malesuada accumsan porttitor.")
+	u.AddOption("--consectetur", "Nullam nisi magna, fermentum sit amet quam id, scelerisque elementum enim.")
+	u.PrintUsages()
 }
---
+++ examples_test.go
@@ -0,0 +1,97 @@
+package fluit_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/qxxt/fluit"
+)
+
+func ExampleUserBreakpoint() {
+	fluit.UserBreakpoint = 20
+	fluit.PrintlnWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis.")
+	fluit.PrintlnWrap(5, strings.Repeat("-+", 5))
+
+	fluit.UserBreakpoint = 60
+	fluit.PrintlnWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis.")
+	fluit.PrintlnWrap(5, strings.Repeat("-+", 20))
+
+	// Output:
+	// Lorem ipsum dolor
+	// sit amet,
+	// consectetur
+	// adipiscing elit.
+	// Vivamus euismod
+	// pharetra sodales.
+	// Aenean ac massa
+	// dictum, gravida nisl
+	// non, fermentum
+	// turpis.
+	//      -+-+-+-+-+
+	// Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	// Vivamus euismod pharetra sodales. Aenean ac massa dictum,
+	// gravida nisl non, fermentum turpis.
+	//      -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+}
+
+func ExampleSprintWrap() {
+	// In essence fluit is a string formatter.
+	str := fluit.SprintWrap(4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis.")
+	fmt.Println(str)
+
+	fmt.Println(fluit.SprintWrap(10, strings.Repeat("-+", 20)))
+
+	// If the margin is set to 0, it will not be margined but still wrapped according to breakpoint.
+	fmt.Println(fluit.SprintWrap(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod pharetra sodales. Aenean ac massa dictum, gravida nisl non, fermentum turpis."))
+
+	// Output:
+	//     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	//     Vivamus euismod pharetra sodales. Aenean ac massa
+	//     dictum, gravida nisl non, fermentum turpis.
+	//           -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	// Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	// Vivamus euismod pharetra sodales. Aenean ac massa dictum,
+	// gravida nisl non, fermentum turpis.
+}
+
+func ExampleSprintUsage() {
+	fmt.Print(fluit.SprintUsage(10, "--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante."))
+	fmt.Print(fluit.SprintUsage(10, "--build", "Nunc gravida metus non turpis sagittis, at lacinia magna tristique. Suspendisse vel ullamcorper eros. Sed eget lacinia urna."))
+	fmt.Print(fluit.SprintUsage(10, "--install-install", "Cras et tellus dignissim, tempor ligula nec, lobortis dui. In eu enim a urna cursus porta."))
+
+	// Output:
+	//   --help      Lorem ipsum dolor sit amet, consectetur
+	//               adipiscing elit. Integer quis aliquam ante.
+	//   --build     Nunc gravida metus non turpis sagittis, at
+	//               lacinia magna tristique. Suspendisse vel
+	//               ullamcorper eros. Sed eget lacinia urna.
+	//   --install-install
+	//               Cras et tellus dignissim, tempor ligula nec,
+	//               lobortis dui. In eu enim a urna cursus porta.
+}
+
+func ExampleUsages() {
+	var u fluit.Usages
+	u.ArgumentRowLength = 10
+	u.AddOption("--help", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis aliquam ante.")
+	u.AddOption("--build", "Nunc gravida metus non turpis sagittis, at lacinia magna tristique. Suspendisse vel ullamcorper eros. Sed eget lacinia urna.")
+	u.AddOption("--install", "Cras et tellus dignissim, tempor ligula nec, lobortis dui. In eu enim a urna cursus porta.")
+	u.AddOption("--some-very-long-flag", "Vivamus facilisis sagittis tristique. Quisque mattis sed libero id congue. Duis vel elit risus.")
+	u.AddOption("--update", "Proin accumsan orci vel ante consequat, ut facilisis ipsum congue.")
+	u.PrintUsages()
+
+	// Output:
+	//   --help      Lorem ipsum dolor sit amet, consectetur
+	//               adipiscing elit. Integer quis aliquam ante.
+	//   --build     Nunc gravida metus non turpis sagittis, at
+	//               lacinia magna tristique. Suspendisse vel
+	//               ullamcorper eros. Sed eget lacinia urna.
+	//   --install   Cras et tellus dignissim, tempor ligula nec,
+	//               lobortis dui. In eu enim a urna cursus porta.
+	//   --some-very-long-flag
+	//               Vivamus facilisis sagittis tristique. Quisque
+	//               mattis sed libero id congue. Duis vel elit
+	//               risus.
+	//   --update    Proin accumsan orci vel ante consequat, ut
+	//               facilisis ipsum congue.
+}
